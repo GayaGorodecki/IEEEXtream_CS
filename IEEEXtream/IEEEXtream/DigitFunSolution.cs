b05d693b90f8dad09ae38fe665a0e77a2ee65658
GayaGorodecki <58183247+GayaGorodecki@users.noreply.github.com>
2021-02-21 16:45:45 +0200

    Add Digit Fun solution

---
+++ IEEEXtream/IEEEXtream/DigitFunSolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class DigitFunSolution
+{
+    static void Main(String[] args)
+    {
+        List<string> input = getInput();
+
+        foreach (string num in input)
+        {
+            int i = 1;
+            string oldNum = num; // save A0
+            string newNum = oldNum.Length.ToString(); // |A0|
+            while (i > 0)
+            {
+                if (!(oldNum.Equals(newNum)))
+                {
+                    oldNum = newNum; // Ai = |Ai-1|
+                    newNum = oldNum.Length.ToString(); // |Ai|
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    static List<string> getInput()
+    {
+        List<string> input = new List<string>();
+        string line = string.Empty;
+
+        while (!(line = Console.ReadLine()).Equals("END")) // Get input until "END" line
+        {
+            input.Add(line);
+        }
+
+        return input;
+    }
+}
