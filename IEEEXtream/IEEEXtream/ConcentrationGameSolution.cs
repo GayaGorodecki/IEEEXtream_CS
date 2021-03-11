efde4ba949d399234325d1a1ec4083de5cdc66d7
GayaGorodecki <58183247+GayaGorodecki@users.noreply.github.com>
2021-03-11 15:50:02 +0200

    Optimize Concentration Game solution

--- IEEEXtream/IEEEXtream/ConcentrationGameSolution.cs
+++ IEEEXtream/IEEEXtream/ConcentrationGameSolution.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 class pairs
 {
-    public string num;
+    public string card;
     public int i1;
     public int i2;
 }
@@ -16,51 +16,43 @@ class ConcentrationGameSolution
     public static void ConcentrationGame()
     {
         int N = Convert.ToInt32(Console.ReadLine());
-        int t = 0;
         bool foundNext = false;
 
-        Dictionary<string, pairs> seen = new Dictionary<string, pairs>();
-        HashSet<int> matched = new HashSet<int>();
-        HashSet<Tuple<int, int>> checkedPairs = new HashSet<Tuple<int, int>>();
-        HashSet<int> checkedIndex = new HashSet<int>();
+        Dictionary<string, pairs> seen = new Dictionary<string, pairs>(); // To find matches.
+        HashSet<int> matched = new HashSet<int>(); // For not check index that already matched.
+        HashSet<Tuple<int, int>> checkedPairs = new HashSet<Tuple<int, int>>(); // For not check checked pairs again.
+        HashSet<int> checkedIndex = new HashSet<int>(); // For not check index that already checked.
 
         int i1 = 1, i2 = 2;
-        Console.WriteLine(i1 + " " + i2);
+        Console.WriteLine(i1 + " " + i2); // First operation.
+
+        // Add to checked pairs list.
         Tuple<int, int> pair = new Tuple<int, int>(i1, i2);
         Tuple<int, int> samePair = new Tuple<int, int>(i2, i1);
         checkedPairs.Add(pair);
         checkedPairs.Add(samePair);
 
-        string ans = string.Empty;
-        while (!((ans = Console.ReadLine()).Equals("-1")))
+        String ans = String.Empty;
+        while (!((ans = Console.ReadLine()).Equals("-1"))) // Not invalid move.
         {
             if (ans.Equals("MATCH"))
             {
                 matched.Add(i1);
                 matched.Add(i2);
-                t++;
-                if (t == N)
+
+                if (matched.Count == 2 * N) // All card matched -> exit.
                 {
                     Console.WriteLine("-1");
                     break;
-                }
-                foreach (pairs p in seen.Values)
-                {
-                    if (p.i1 != 0 && p.i2 != 0 && !matched.Contains(p.i1)) // match = list<pairs> ??
-                    {
-                        i1 = p.i1;
-                        i2 = p.i2;
-                        foundNext = true;
-                        break;
-                    }
                 }
             }
             else
             {
                 string[] nums = ans.Split();
-                if (seen.ContainsKey(nums[0]) || seen.ContainsKey(nums[1]))
+                if (seen.ContainsKey(nums[0]) || seen.ContainsKey(nums[1])) // Update index of cards -> get next match.
                 {
-                    foreach (KeyValuePair<string,pairs> p in seen)
+                    bool updateFirst = false, updateSecond = false;
+                    foreach (KeyValuePair<string, pairs> p in seen)
                     {
                         if (p.Key == nums[0])
                         {
@@ -72,7 +64,7 @@ class ConcentrationGameSolution
                             {
                                 p.Value.i1 = i1;
                             }
-                            break;
+                            updateFirst = true;
                         }
                         else if (p.Key == nums[1])
                         {
@@ -84,14 +76,19 @@ class ConcentrationGameSolution
                             {
                                 p.Value.i1 = i2;
                             }
+                            updateSecond = true;
+                        }
+                        if (updateFirst == true && updateSecond == true)
+                        {
                             break;
                         }
                     }
                 }
-                if (!seen.ContainsKey(nums[0]))
+
+                if (!seen.ContainsKey(nums[0])) // If not seen - add new seen card.
                 {
                     pairs newPair = new pairs();
-                    newPair.num = nums[0];
+                    newPair.card = nums[0];
                     newPair.i1 = i1;
                     seen.Add(nums[0], newPair);
                     checkedIndex.Add(i1);
@@ -99,61 +96,28 @@ class ConcentrationGameSolution
                 if (!seen.ContainsKey(nums[1]))
                 {
                     pairs newPair = new pairs();
-                    newPair.num = nums[1];
+                    newPair.card = nums[1];
                     newPair.i2 = i2;
                     seen.Add(nums[1], newPair);
                     checkedIndex.Add(i2);
                 }
+            }
 
-                foreach (pairs p in seen.Values)
+            foreach (pairs p in seen.Values) // Search for more matches.
+            {
+                if (p.i1 != 0 && p.i2 != 0 && !matched.Contains(p.i1)) // If there is a pair with two indexes.
                 {
-                    if (p.i1 != 0 && p.i2 != 0 && !matched.Contains(p.i1))
-                    {
-                        i1 = p.i1;
-                        i2 = p.i2;
-                        foundNext = true;
-                        break;
-                    }
-                }
-
-                if (foundNext == false)
-                {
-                    foreach (pairs p in seen.Values)
-                    {
-                        if (p.num == nums[0] && p.i1 != i1 && p.i2 != i1)
-                        {
-                            if (p.i1 != 0)
-                            {
-                                i2 = p.i1;
-                            }
-                            else if (p.i2 != 0)
-                            {
-                                i2 = p.i2;
-                            }
-                            foundNext = true;
-                            break;
-                        }
-                        else if (p.num == nums[1] && p.i1 != i2 && p.i2 != i2)
-                        {
-                            if (p.i1 != 0)
-                            {
-                                i1 = p.i1;
-                            }
-                            else if (p.i2 != 0)
-                            {
-                                i1 = p.i2;
-                            }
-                            foundNext = true;
-                            break;
-                        }
-                    }
+                    i1 = p.i1;
+                    i2 = p.i2;
+                    foundNext = true;
+                    break;
                 }
             }
 
-            if (foundNext == false)
+            if (foundNext == false) // If not found another match.
             {
                 int i = 1;
-                while (checkedPairs.Contains(pair))
+                while (checkedPairs.Contains(pair)) // Search for next indexes that not checked.
                 {
                     while ((checkedIndex.Contains(i) || matched.Contains(i)) && i < 2 * N - 1)
                     {
